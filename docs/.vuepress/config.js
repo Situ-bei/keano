5af52a53d0a79088840d577c7fd418a3aa10bc87
situ-bei <2606405907@qq.com>
2024-10-08 20:10:31 +0800

    修改base Url

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
-
+  base: '/keano/',
   title: 'VuePress',
   description: 'My first VuePress Site',
 
