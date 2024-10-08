d7f4e57f2eb77a6a9d0b0c3060574c70f3118be6
situ-bei <2606405907@qq.com>
2024-10-08 21:24:51 +0800

    更改主题_【hope】

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ import { blogPlugin } from '@vuepress/plugin-blog'
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
+import { hopeTheme } from "vuepress-theme-hope";
 
 export default defineUserConfig({
   lang: 'zh-CN',
@@ -9,30 +10,9 @@ export default defineUserConfig({
   title: 'VuePress',
   description: 'My first VuePress Site',
 
-  theme: defaultTheme({
-    logo: 'https://vuejs.press/images/hero.png',
-
-    navbar: [
-      '/',
-      {
-        text: 'Article',
-        link: '/article/',
-      },
-      {
-        text: 'Category',
-        link: '/category/',
-      },
-      {
-        text: 'Tag',
-        link: '/tag/',
-      },
-      {
-        text: 'Timeline',
-        link: '/timeline/',
-      },
-    ],
+  theme: hopeTheme({
+    // 此处放置主题配置
   }),
-
   plugins: [
     blogPlugin({
       // Only files under posts are articles
