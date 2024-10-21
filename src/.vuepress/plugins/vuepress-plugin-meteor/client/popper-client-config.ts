936e6ea779579a932d4f0cfb1a6dda14f3fae219
situ-bei <2606405907@qq.com>
2024-10-21 21:40:45 +0800

    删除

--- src/.vuepress/plugins/vuepress-plugin-meteor/client/popper-client-config.ts
+++
@@ -1,6 +0,0 @@
-import { defineClientConfig, usePageData, useSiteData } from "vuepress/client";
-import { MeteorMoefyCanvas } from "../client/components/MeteorMoefyCanvas.js"
-
-export default defineClientConfig({
-  rootComponents: [MeteorMoefyCanvas],
-});
--- src/.vuepress/plugins/vuepress-plugin-meteor/type.ts
+++
@@ -1,7 +0,0 @@
-import type { CanvasOptions } from '@moefy-canvas/core'
-import type { MeteorConfig } from "@moefy-canvas/theme-meteor"
-
-export interface MeteorOption {
-   config: MeteorConfig ;
-   canvasOptions?:CanvasOptions
-}
