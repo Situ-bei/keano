c6b9738bf1eeb09fdd2d3b76e82093dd0e1086e2
Situ-bei <62317522+Situ-bei@users.noreply.github.com>
2024-10-24 01:02:26 +0000

    Deploying to blog_pages from @ Situ-bei/keano@d2e39434828066bfcb7b341ab2ed558f3ded3ad3 🚀

---
+++ assets/BlogBeautify-CwQK_O9I.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as s,m as a,g as l,a as r}from"./app-CI2Mbx1E.js";const c={};function _(f,e){const n=t("ClientOnly");return s(),a(n,null,{default:l(()=>e[0]||(e[0]=[r("div",{class:"none"},"主题美化",-1)])),_:1})}const u=o(c,[["render",_],["__file","BlogBeautify.vue"]]);export{u as default};
---
+++ assets/HeroHitokoto-BN09_L04.js
@@ -0,0 +1,15 @@
+import{j as g,J as y,X as k,p as E,Y as T,_ as H,r as I,o as B,m as w,g as C,a as $}from"./app-CI2Mbx1E.js";const M=g({__name:"HeroHitokoto",setup(x,{expose:i}){i();let r="https://api.apiopen.top/api/sentences";const m=t=>{const e=window;clearInterval(e.Timer);const o=document.getElementsByClassName("vp-blog-hero-description");if(o.length>0){const f=o[0];f.id="hotWord"}const l=document.getElementById("hotWord");if(!l)return;l.innerHTML=`
+    <div class="word">
+      <div class="left">
+        
+      </div>
+        <span id="hitokoto_text">
+        </span>
+      <div class="right">
+        
+      </div>
+    </div>
+    <div class="author" id="hitokoto_author"> 
+      ——《${t.from}》
+    </div>
+  `;const c=document.getElementById("hitokoto_author"),_=document.getElementById("hitokoto_text");if(!_||!c)return;c.style.opacity="0";const h=t.hitokoto.split("");let n=0,a=0,v=60,u=!1,d="";e.Timer=setInterval(()=>{const f=h[n];if(d.length<4?c.style.opacity="0":c.style.opacity="1",a!=0){a--,d.length==0&&a==0&&clearInterval(e.Timer);return}u?clearInterval(e.Timer):d+=f,u?n--:n++,n>=h.length&&(u=!0,n=h.length,a=v),n<0&&(u=!1,n=0,a=v),_.innerHTML=d},200)},s=t=>{let e=window.location.pathname;t&&(e=t),e=="/"&&T({method:"get",url:r,params:{}}).then(o=>{console.log(o.data,"主页每日一句");const l={hitokoto:o.data.result.name,from:o.data.result.from};m(l)}).catch(o=>{r="https://international.v1.hitokoto.cn",s()})};y(()=>{k(()=>{s()}),E().afterEach(e=>{k(()=>{s(e.fullPath)})})});const p={get hitokotoUrl(){return r},set hitokotoUrl(t){r=t},InsertText:m,GetWord:s};return Object.defineProperty(p,"__isScriptSetup",{enumerable:!1,value:!0}),p}});function W(x,i,r,m,s,p){const t=I("ClientOnly");return B(),w(t,null,{default:C(()=>i[0]||(i[0]=[$("div",{class:"none"},"一言插件",-1)])),_:1})}const N=H(M,[["render",W],["__file","HeroHitokoto.vue"]]);export{N as default};
