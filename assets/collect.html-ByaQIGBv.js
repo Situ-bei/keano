30a1f839d8ce5d937181ec3c241ac3a1720d94e0
Situ-bei <62317522+Situ-bei@users.noreply.github.com>
2024-10-29 10:17:52 +0000

    Deploying to blog_pages from @ Situ-bei/keano@4b014d6a351f0280030aa63103e58c804c88eee3 🚀

---
+++ assets/collect.html-ByaQIGBv.js
@@ -0,0 +1,16 @@
+import{_ as e,c as t,b as n,o as a}from"./app-GcNVbVkX.js";const o={};function l(c,i){return a(),t("div",null,[n(` ---
+date: 2024-10-22
+title: 收藏
+icon: icon-shoucang
+index: false
+timeline: false
+article: false
+sidebar: false
+breadcrumb: false # 路径导航
+pageInfo: false # 页面信息
+comment: false # 评论
+headerDepth: 5
+toc: false
+lastUpdated: false
+contributors: false
+--- `)])}const r=e(o,[["render",l],["__file","collect.html.vue"]]),m=JSON.parse('{"path":"/collect.html","title":"收藏","lang":"zh-CN","frontmatter":{"layout":"MyCollect","title":"收藏","index":false,"timeline":false,"article":false,"head":[["meta",{"property":"og:url","content":"https://situ-bei.github.io/keano/collect.html"}],["meta",{"property":"og:site_name","content":"icyn"}],["meta",{"property":"og:title","content":"收藏"}],["meta",{"property":"og:type","content":"website"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-10-29T10:07:06.000Z"}],["meta",{"property":"article:modified_time","content":"2024-10-29T10:07:06.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"WebPage\\",\\"name\\":\\"收藏\\"}"]]},"headers":[],"git":{"createdTime":1729731654000,"updatedTime":1730196426000,"contributors":[{"name":"situ-bei","email":"2606405907@qq.com","commits":2}]},"readingTime":{"minutes":0.18,"words":53},"filePathRelative":"collect.md","localizedDate":"2024年10月24日","excerpt":"<!-- ---\\ndate: 2024-10-22\\ntitle: 收藏\\nicon: icon-shoucang\\nindex: false\\ntimeline: false\\narticle: false\\nsidebar: false\\nbreadcrumb: false # 路径导航\\npageInfo: false # 页面信息\\ncomment: false # 评论\\nheaderDepth: 5\\ntoc: false\\nlastUpdated: false\\ncontributors: false\\n--- -->\\n"}');export{r as comp,m as data};
