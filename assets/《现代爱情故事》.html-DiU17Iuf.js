fac5cfead8cf936129091812edec19b58a455e99
Situ-bei <62317522+Situ-bei@users.noreply.github.com>
2024-11-08 01:31:04 +0000

    Deploying to blog_pages from @ Situ-bei/keano@3e132d4f0e3828d034830e7104240111162e2585 🚀

---
+++ assets/《现代爱情故事》.html-DiU17Iuf.js
@@ -0,0 +1,24 @@
+import{_ as e,c as t,b as l,g as n,e as i,f as r,r as a,o as p,d as o}from"./app-B6uUf-bH.js";const c=e({},[["render",function(e,c){const u=a("Tabs");return p(),t("div",null,[c[4]||(c[4]=l("p",null,"粤语歌曲谐音学唱--《现代爱情故事》",-1
+/* HOISTED */)),n(" more "),c[5]||(c[5]=l("div",{style:{"text-align":"center"}},[l("h3",{id:"《现代爱情故事》",tabindex:"-1"},[l("a",{class:"header-anchor",href:"#《现代爱情故事》"},[l("span",null,"《现代爱情故事》")])])],-1
+/* HOISTED */)),i(u,{id:"9",data:[{id:"前调"},{id:"高潮"}]},{title0:r((({value:e,isActive:t})=>c[0]||(c[0]=[o("前调")]))),title1:r((({value:e,isActive:t})=>c[1]||(c[1]=[o("高潮")]))),tab0:r((({value:e,isActive:t})=>c[2]||(c[2]=[l("p",null,[o("女：别离没有对错"),l("br"),o(" （女：比类木腰对错）")],-1
+/* HOISTED */),l("p",null,[o("要走也解释不多"),l("br"),o(" （腰早 牙改sei巴多）")],-1
+/* HOISTED */),l("p",null,[o("现代说永远已经很傻"),l("br"),o(" （引带虚 wing云 已ging很索）")],-1
+/* HOISTED */),l("p",null,[o("随着那一宵去火花已消逝"),l("br"),o(" （翠这那 亚修灰 佛发已休塞）")],-1
+/* HOISTED */),l("p",null,[o("不可能付出一生那么多"),l("br"),o(" （巴活能付戳 亚伤那摸多）")],-1
+/* HOISTED */),l("p",null,[o("男：情尽时就要放过"),l("br"),o(" （男：情尊细照优放过）")],-1
+/* HOISTED */),l("p",null,[o("我怎会想穿心窝"),l("br"),o(" （我赞微赏群桑窝）")],-1
+/* HOISTED */),l("p",null,[o("若是厌弃了再不蹉跎"),l("br"),o(" （约系 引黑流 拽八蹉跎）")],-1
+/* HOISTED */),l("p",null,[o("如共你分开应有机会在爱一个"),l("br"),o(" （鱼共内分嗨 应瑶给微 拽爱呀锅）")],-1
+/* HOISTED */),l("p",null,[o("不可能付出一生空虚过"),l("br"),o(" （巴活nan 夫戳呀伤 轰灰过）")],-1
+/* HOISTED */)]))),tab1:r((({value:e,isActive:t})=>c[3]||(c[3]=[l("p",null,[o("合：你我情如路半经过"),l("br"),o(" （合：内我 情鱼漏bun ging过）")],-1
+/* HOISTED */),l("p",null,[o("深知道再爱痛苦必多"),l("br"),o(" （伤基兜 拽爱 痛服必多）")],-1
+/* HOISTED */),l("p",null,[o("愿你可轻轻松松放低我"),l("br"),o(" （晕内活 hing hing松松 放呆我）")],-1
+/* HOISTED */),l("p",null,[o("剩了些开心的追忆送走我"),l("br"),o(" （兴流sei 嗨桑得 追一送早我）")],-1
+/* HOISTED */),l("p",null,[o("皆因了解之后认清楚"),l("br"),o(" （该央 流改机号 硬清戳）")],-1
+/* HOISTED */),l("p",null,[o("离别时笑笑明辰剩我一个"),l("br"),o(" （类比细 秀秀明声 兴我呀锅）")],-1
+/* HOISTED */),l("p",null,[o("潇洒里也会记起当初"),l("br"),o(" （休洒雷 牙微 给嘿东磋）")],-1
+/* HOISTED */),l("p",null,[o("女：若你的心中孤单再找我"),l("br"),o(" （女：约内得桑中 孤单 拽找我）")],-1
+/* HOISTED */),l("p",null,[o("男：若你的心窝中空虚再找我"),l("br"),o(" （男：约内得桑窝中 轰灰 拽找我）")],-1
+/* HOISTED */),l("p",null,[o("合：不必痛苦当忆起我"),l("br"),o(" （合：巴必痛服东一嘿我）")],-1
+/* HOISTED */)]))),_:1
+/* STABLE */})])}],["__file","《现代爱情故事》.html.vue"]]),u=JSON.parse('{"path":"/life/music/%E3%80%8A%E7%8E%B0%E4%BB%A3%E7%88%B1%E6%83%85%E6%95%85%E4%BA%8B%E3%80%8B.html","title":"《现代爱情故事》","lang":"zh-CN","frontmatter":{"date":"2024-06-29T00:00:00.000Z","title":"《现代爱情故事》","category":["生活","音乐"],"tag":["粤语歌曲","学唱","《现代爱情故事》"],"description":"粤语歌曲谐音学唱--《现代爱情故事》","head":[["link",{"rel":"alternate","hreflang":"en-us","href":"https://situ-bei.github.io/keano/en/life/music/%E3%80%8A%E7%8E%B0%E4%BB%A3%E7%88%B1%E6%83%85%E6%95%85%E4%BA%8B%E3%80%8B.html"}],["meta",{"property":"og:url","content":"https://situ-bei.github.io/keano/life/music/%E3%80%8A%E7%8E%B0%E4%BB%A3%E7%88%B1%E6%83%85%E6%95%85%E4%BA%8B%E3%80%8B.html"}],["meta",{"property":"og:site_name","content":"icyn"}],["meta",{"property":"og:title","content":"《现代爱情故事》"}],["meta",{"property":"og:description","content":"粤语歌曲谐音学唱--《现代爱情故事》"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:locale:alternate","content":"en-US"}],["meta",{"property":"og:updated_time","content":"2024-10-22T04:51:14.000Z"}],["meta",{"property":"article:tag","content":"粤语歌曲"}],["meta",{"property":"article:tag","content":"学唱"}],["meta",{"property":"article:tag","content":"《现代爱情故事》"}],["meta",{"property":"article:published_time","content":"2024-06-29T00:00:00.000Z"}],["meta",{"property":"article:modified_time","content":"2024-10-22T04:51:14.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"《现代爱情故事》\\",\\"image\\":[\\"\\"],\\"datePublished\\":\\"2024-06-29T00:00:00.000Z\\",\\"dateModified\\":\\"2024-10-22T04:51:14.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"icyn\\",\\"url\\":\\"https://situ-bei.github.io/keano/\\"}]}"]]},"headers":[],"git":{"createdTime":1729242891000,"updatedTime":1729572674000,"contributors":[{"name":"situ-bei","email":"2606405907@qq.com","commits":3}]},"readingTime":{"minutes":1.47,"words":440},"filePathRelative":"life/music/《现代爱情故事》.md","localizedDate":"2024年6月29日","excerpt":"<p>粤语歌曲谐音学唱--《现代爱情故事》</p>\\n","autoDesc":true}');export{c as comp,u as data};
